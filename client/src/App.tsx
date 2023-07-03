5c87b9d6ef2e0e798c164843a5860c485d853487
LeeJSYS <dauzzang@gmail.com>
2023-07-03 16:22:43 +0900

    Chore: Global Style Component use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import Header from './components/layout/Header.tsx';
 import Footer from './components/layout/Footer.tsx';
 import PageRouter from './pages/PageRouter.tsx';
 import './App.css';
+import GlobalStyle from './styles/GlobalStyle.tsx';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <PageRouter />
       <Footer />
