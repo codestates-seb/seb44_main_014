4a4b4dd880ff00d4a286c4da1d85d6767e1d50f9
조성원 <77275383+thejsw@users.noreply.github.com>
2023-07-03 16:23:46 +0900

    Revert "Chore: Global Style Component use"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,10 @@ import Header from './components/layout/Header.tsx';
 import Footer from './components/layout/Footer.tsx';
 import PageRouter from './pages/PageRouter.tsx';
 import './App.css';
-import GlobalStyle from './styles/GlobalStyle.tsx';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Header />
       <PageRouter />
       <Footer />
