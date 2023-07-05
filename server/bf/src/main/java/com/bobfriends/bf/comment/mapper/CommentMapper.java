e74c7f44a46229e55be09b52a0006f6a301a2a56
sseung <sseung@skuniv.ac.kr>
2023-07-05 14:20:22 +0900

    fix: pull 받기 위한 커밋

--- server/bf/src/main/java/com/bobfriends/bf/comment/mapper/CommentMapper.java
+++ server/bf/src/main/java/com/bobfriends/bf/comment/mapper/CommentMapper.java
@@ -4,17 +4,15 @@ import com.bobfriends.bf.comment.dto.CommentDto;
 import com.bobfriends.bf.comment.entity.Comment;
 import com.bobfriends.bf.question.service.QuestionService;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CommentMapper {
-    default Comment commentPostToComment(long questionId, QuestionService questionService, CommentDto.Post requestBody){
-        Comment comment = new Comment();
-        comment.setContent(requestBody.getContent());
-        comment.setQuestion(questionService.findVerifiedQuestion(questionId));
-        return comment;
-    }
+    @Mapping(source="memberId",target = "member.memberId")
+    @Mapping(source="questionId",target = "question.questionId")
     Comment commentPostToComment(CommentDto.Post post);
     Comment commentPatchToComment(CommentDto.Patch patch);
+
     CommentDto.Response commentToCommentResponse(Comment comment);
 }
