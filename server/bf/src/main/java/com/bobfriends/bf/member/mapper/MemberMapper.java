b3ef0bcebe322a5f75345136ecc462215323c644
bomi <spring3310@naver.com>
2023-07-06 11:45:10 +0900

    feat : patchMember 구현

--- server/bf/src/main/java/com/bobfriends/bf/member/mapper/MemberMapper.java
+++ server/bf/src/main/java/com/bobfriends/bf/member/mapper/MemberMapper.java
@@ -3,7 +3,10 @@ package com.bobfriends.bf.member.mapper;
 import com.bobfriends.bf.member.dto.MemberDto;
 import com.bobfriends.bf.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+
+import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MemberMapper {
@@ -22,46 +25,12 @@ public interface MemberMapper {
         return member;
     }
 
-    default Member memberPatchToMember(MemberDto.Patch requestBody) {
-        if (requestBody == null) {
-            return null;
-        }
+    Member memberPatchDtoToMember(MemberDto.Patch requestBody);
 
-        Member member = new Member();
+    @Mapping(source = "member.memberId", target = "memberId")
+    @Mapping(source = "member.name", target = "name")
+    @Mapping(source = "member.avgStarRate", target = "avgStarRate")
+    @Mapping(source = "member.gender", target = "gender")
+    MemberDto.Response memberToMemberResponseDto(Member member);
 
-        member.setName(requestBody.getName());
-        member.setMemberId(requestBody.getMemberId());
-        member.setPassword(requestBody.getPassword());
-
-        return member;
-    }
-
-    default MemberDto.Response memberToMemberResponseDto(Member member) {
-
-        boolean eatStatus = member.isEatStatus();
-
-        String genderStatusString = member.getGender().getStatus();  // genderStatus 값을 String으로 가져옴
-        Member.genderStatus gender = null;
-
-        if (genderStatusString.equals(Member.genderStatus.FEMALE.getStatus())) {
-            gender = Member.genderStatus.FEMALE;
-        } else if (genderStatusString.equals(Member.genderStatus.MALE.getStatus())) {
-            gender = Member.genderStatus.MALE;
-        }
-
-        MemberDto.Response response = new MemberDto.Response(
-                member.getMemberId(),
-                member.getImage(),
-                member.getName(),
-                member.getEmail(),
-                gender,  // String 값을 genderStatus 타입으로 변환한 변수를 사용
-                member.getLocation(),
-                eatStatus,
-                member.getAvgStarRate()
-
-        );
-
-
-        return response;
-    }
 }
