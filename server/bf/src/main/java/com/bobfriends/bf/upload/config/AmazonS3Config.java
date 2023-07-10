6d9d324f4672e90df729b8cefc97ea897438450e
jmjgirl <jmjcap123@gmail.com>
2023-07-10 11:59:54 +0900

    Feat: 이미지 S3 업로드

---
+++ server/bf/src/main/java/com/bobfriends/bf/upload/config/AmazonS3Config.java
@@ -0,0 +1,31 @@
+package com.bobfriends.bf.upload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {
+
+    /*
+    @Value(("${cloud.aws.credentials.access-key}"))
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(region)
+                .build();
+    }
+     */
+}
