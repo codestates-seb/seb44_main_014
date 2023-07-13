a72847b622fc874c6b1b1a62577ed2d1085181d7
jmjgirl <jmjcap123@gmail.com>
2023-07-13 09:14:54 +0900

    Deploy: 환경 설정

--- server/bf/src/test/java/com/bobfriends/bf/post/controller/PostControllerTest.java
+++ server/bf/src/test/java/com/bobfriends/bf/post/controller/PostControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
