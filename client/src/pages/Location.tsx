e5e06c37403be32c9ffe56791174713dd4d075b9
LeeJSYS <dauzzang@gmail.com>
2023-07-23 16:04:54 +0900

    Fix: Location page 기본 위치 수정

--- client/src/pages/Location.tsx
+++ client/src/pages/Location.tsx
@@ -16,7 +16,7 @@ const Location = () => {
     const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(37.55614088474113, 126.9358131381049), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(37.563115742175256, 126.9226833612614), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
