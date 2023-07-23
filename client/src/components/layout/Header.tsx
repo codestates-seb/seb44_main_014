226c31725359524a5f75c3cac8819face2ab0f2e
thejsw <jswtncapetnc@gmail.com>
2023-07-23 15:01:42 +0900

    feat:로그인 여부에 따라 header 변경

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,23 +1,57 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { IUserState } from '../../store/userSlice.ts';
+import HeaderNavLogin from './HeaderNavLogin.tsx';
+import HeaderNavLogout from './HeaderNavLogout.tsx';
+import Logout from '../Logout/Logout.tsx';
 
 const Header = () => {
+  const Loginstate = useSelector((state: IUserState) => {
+    state.user.IsLogin;
+  });
+
+  const [isClicked, setIsClicked] = useState(true);
+
+  const ClickHandler = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <HeaderContainer>
-      <HeaderPositioner>
-        <HeaderLeftContents>
-          <LocationIcon icon={faLocationDot} />
-        </HeaderLeftContents>
-        <HeaderTitle># 밥친구</HeaderTitle>
-        <HeaderRightContents>
-          <HeaderRightItems>보드</HeaderRightItems>
-          <HeaderRightItems>마이페이지</HeaderRightItems>
-          <HeaderRightItems>로그아웃</HeaderRightItems>
-          <HeaderHamburgerIcon icon={faBars} />
-        </HeaderRightContents>
-      </HeaderPositioner>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <HeaderPositioner>
+          <HeaderLeftContents>
+            <Link to="/location">
+              <LocationIcon icon={faLocationDot} />
+            </Link>
+          </HeaderLeftContents>
+          <HeaderTitleContainer>
+            <Link to="/">
+              <HeaderTitle># 밥친구</HeaderTitle>
+            </Link>
+          </HeaderTitleContainer>
+          <HeaderRightContents>
+            <Link to="/board">
+              <HeaderRightItems>보드</HeaderRightItems>
+            </Link>
+            <Link to="/users/mypage/:memberId">
+              <HeaderRightItems>마이페이지</HeaderRightItems>
+            </Link>
+            <HeaderRightItems>{Loginstate ? <Logout /> : <StyledLink to="/Login">로그인</StyledLink>}</HeaderRightItems>
+            {isClicked ? (
+              <HeaderHamburgerIcon icon={faBars} onClick={ClickHandler} />
+            ) : (
+              <HeaderXIcon icon={faX} onClick={ClickHandler} />
+            )}
+          </HeaderRightContents>
+        </HeaderPositioner>
+      </HeaderContainer>
+      {!isClicked && Loginstate ? <HeaderNavLogout /> : <HeaderNavLogin />}
+    </>
   );
 };
 
@@ -25,11 +59,12 @@ const HeaderContainer = styled.header`
   position: sticky;
   left: 0;
   top: 0;
+  z-index: 10;
   width: 100%;
   height: 4.375rem;
   background-color: var(--color-green);
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     height: 3.125rem;
   }
 `;
@@ -58,22 +93,25 @@ const HeaderLeftContents = styled.div`
   display: flex;
   width: 16.25rem;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     width: 1.875rem;
   }
 `;
 
-const HeaderTitle = styled.h1`
+const HeaderTitleContainer = styled.h1`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+const HeaderTitle = styled.h1`
   padding-top: 0.3125rem;
   color: #fff;
   font-family: Tenada;
   font-size: 2rem;
   font-weight: bold;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     font-size: 1.5rem;
   }
 `;
@@ -83,7 +121,7 @@ const HeaderRightContents = styled.div`
   justify-content: flex-end;
   width: 16.25rem;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     width: 1.875rem;
   }
 `;
@@ -93,7 +131,7 @@ const HeaderRightItems = styled.div`
   color: #fff;
   font-size: 1rem;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     display: none;
   }
 `;
@@ -104,9 +142,24 @@ const HeaderHamburgerIcon = styled(FontAwesomeIcon)`
   height: 1.5625rem;
   color: #fff;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     display: block;
   }
+`;
+
+const HeaderXIcon = styled(FontAwesomeIcon)`
+  display: none;
+  width: 1.5625rem;
+  height: 1.5625rem;
+  color: #fff;
+
+  @media screen and (max-width: 1024px) {
+    display: block;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: #fff;
 `;
 
 export default Header;
