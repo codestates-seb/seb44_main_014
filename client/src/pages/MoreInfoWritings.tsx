c6f8a32e5310d70b7875e45d98a0c00f59f6ea71
LeeJSYS <dauzzang@gmail.com>
2023-07-24 23:32:08 +0900

    Fix: postscontent 줄넘어가면 생략

--- client/src/pages/MoreInfoWritings.tsx
+++ client/src/pages/MoreInfoWritings.tsx
@@ -87,6 +87,11 @@ const ContentsTitle = styled.h1`
 
 const ContentsParagraph = styled.p`
   font-size: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
 `;
 
 const ContentsMetting = styled.p`
